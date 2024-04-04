c8293b246e5bb79d81f571f3da6c8827d467cca7
agakshita5 <akshita.ag2020@gmail.com>
2024-04-04 16:25:44 +0530

    Updated javascript file

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
-const inputTask=document.getElementById("input-task");
-const addBtn=document.getElementById("add-btn");
-const taskList=document.querySelector(".task-list-container");
-const form=document.querySelector("form");
-const body=document.body;
+const inputTask = document.getElementById("input-task");
+const addBtn = document.getElementById("add-btn");
+const taskList = document.querySelector(".task-list-container");
+const ol = document.querySelector("ol");
+const body = document.body;
 
 let taskIdCounter = 0;
 
-addBtn.addEventListener("click",()=>{
-    let task=inputTask.value;
+addBtn.addEventListener("click", () => {
+  let task = inputTask.value;
 
-    addTask(task);
+  addTask(task);
 });
 
-const addTask=(task)=>{
-    taskIdCounter++;
-    const checkbox = document.createElement("input");
-    checkbox.type="checkbox";
-    checkbox.id=`task-checkbox-${taskIdCounter}`;
-    checkbox.name=task;
-    checkbox.value=task;
+const addTask = (task) => {
+  taskIdCounter++;
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.id = `task-checkbox-${taskIdCounter}`;
+  checkbox.name = task;
+  checkbox.value = task;
 
-    const label=document.createElement("label");
-    label.innerText=task;
-    label.classList.add("task-label");
-    label.setAttribute("for",checkbox.id);
+  const label = document.createElement("label");
+  label.innerText = task;
+  label.classList.add("task-label");
+  label.setAttribute("for", checkbox.id);
 
-    form.appendChild(checkbox);
-    form.appendChild(label);
+  const listItem = document.createElement("li");
+  listItem.append(checkbox);
+  listItem.append(label);
+  ol.append(listItem);
 
-    inputTask.value="";
-}
+  inputTask.value = "";
 
-const checkboxes=document.querySelectorAll("input[type='checkbox']");
-const labels=document.querySelectorAll("label");
-console.log(checkboxes[0]);
+  checkbox.addEventListener("change",()=>{
+    label.classList.toggle("strikethrough",this.checked);
+  })
 
-// checkboxes.forEach((checkbox)=>{
-//     if(checkbox.checked){
-//         console.log("y");
-//     }
-// });
+};
+
+
